e03ea821a2c4dca15bae4a458b599e82a6de645e
Nick Wong <njwon4@gmail.com>
2022-10-24 22:39:56 +1100

    chore: add eslint and prettier

--- pages/lambda-test.tsx
+++ pages/lambda-test.tsx
@@ -14,49 +14,55 @@ const LambdaTest = () => {
   const [usernameInput, setUsernameInput] = useState('')
   const [eventLog, setEventLog] = useState<Event[]>([])
 
-  const { isLoading, error, data, refetch, isFetching } = useQuery(['getUsers'], async () => {
-    const start = new Date().valueOf()
-    const res = await fetch('/api/lambda/getUsers')
-    const end = new Date().valueOf()
-    const data: GetUsersData = await res.json()
+  const { isLoading, error, data, refetch, isFetching } = useQuery(
+    ['getUsers'],
+    async () => {
+      const start = new Date().valueOf()
+      const res = await fetch('/api/lambda/getUsers')
+      const end = new Date().valueOf()
+      const data: GetUsersData = await res.json()
 
-    const newEvent: Event = {
-      name: 'getUsers',
-      start,
-      end,
-    }
+      const newEvent: Event = {
+        name: 'getUsers',
+        start,
+        end,
+      }
 
-    setEventLog(prev => [...prev, newEvent])
+      setEventLog((prev) => [...prev, newEvent])
 
-    return data
-  })
+      return data
+    },
+  )
 
-  const { refetch: getMockData } = useQuery(['getMockData'], async () => {
-    const start = new Date().valueOf()
-    const res = await fetch('/api/lambda/getUsers')
-    const end = new Date().valueOf()
-    const data = await res.json()
+  const { refetch: getMockData } = useQuery(
+    ['getMockData'],
+    async () => {
+      const start = new Date().valueOf()
+      const res = await fetch('/api/lambda/getUsers')
+      const end = new Date().valueOf()
+      const data = await res.json()
 
-    const newEvent: Event = {
-      name: 'getMockData',
-      start,
-      end,
-    }
+      const newEvent: Event = {
+        name: 'getMockData',
+        start,
+        end,
+      }
 
-    setEventLog(prev => [...prev, newEvent])
+      setEventLog((prev) => [...prev, newEvent])
 
-    return data
-  }, { enabled: false})
+      return data
+    },
+    { enabled: false },
+  )
 
-  const {
-    isLoading: createIsLoading,
-    mutate: createUser
-  } = useMutation((newUser: CreateUserInput) => {
-    return fetch('/api/lambda/createUser', {
-      method: 'POST',
-      body: JSON.stringify(newUser),
-    })
-  })
+  const { isLoading: createIsLoading, mutate: createUser } = useMutation(
+    (newUser: CreateUserInput) => {
+      return fetch('/api/lambda/createUser', {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+      })
+    },
+  )
 
   const handleMockRequest = () => {
     getMockData()
@@ -65,7 +71,9 @@ const LambdaTest = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const start = new Date().valueOf()
-    createUser({ name: usernameInput }, {
+    const newUser = { name: usernameInput }
+
+    createUser(newUser, {
       onSuccess: () => {
         const end = new Date().valueOf()
         const newEvent: Event = {
@@ -73,10 +81,10 @@ const LambdaTest = () => {
           start,
           end,
         }
-        setEventLog(prev => [...prev, newEvent])
+        setEventLog((prev) => [...prev, newEvent])
 
         queryClient.invalidateQueries(['getUsers'])
-      }
+      },
     })
   }
 
@@ -95,33 +103,56 @@ const LambdaTest = () => {
       <div className="grid gap-4">
         <form onSubmit={handleSubmit} className="grid gap-4">
           <div className="grid gap-1">
-            <label htmlFor="username" className="text-sm">Name</label>
-            <input onChange={(e) => { setUsernameInput(e.target.value) }} type="text" name="username" id="username" className="border border-slate-400 px-4 py-2 rounded" />
+            <label htmlFor="username" className="text-sm">
+              Name
+            </label>
+            <input
+              onChange={(e) => {
+                setUsernameInput(e.target.value)
+              }}
+              type="text"
+              name="username"
+              id="username"
+              className="rounded border border-slate-400 py-2 px-4"
+            />
           </div>
-          <button disabled={createIsLoading} type="submit" className="bg-indigo-500 text-white px-4 py-2 rounded disabled:bg-indigo-300 disabled:cursor-not-allowed">Create User</button>
+          <button
+            disabled={createIsLoading}
+            type="submit"
+            className="roundedbg-indigo-500 px-4 py-2 text-white disabled:cursor-not-allowed disabled:bg-indigo-300"
+          >
+            Create User
+          </button>
         </form>
         {/* Users listing */}
         <div>
           <p>Users:</p>
-          {!data && (
-            <p>No users</p>
-          )}
+          {!data && <p>No users</p>}
           {data && (
-            <ul className="list-disc ml-8">
-              {data.users.map(user => (
+            <ul className="ml-8 list-disc">
+              {data.users.map((user) => (
                 <li key={user.id}>{user.name}</li>
               ))}
             </ul>
           )}
         </div>
-        <button onClick={() => { refetch() }} className="text-indigo-400 rounded border-2 border-indigo-400 px-4 py-2">{ isFetching ? 'Refetching...' : 'Refetch users'}</button>
+        <button
+          onClick={() => {
+            refetch()
+          }}
+          className="rounded border-2 border-indigo-400 px-4 py-2 text-indigo-400"
+        >
+          {isFetching ? 'Refetching...' : 'Refetch users'}
+        </button>
       </div>
       {/* Event log */}
       <div>
         <h2>Log</h2>
         <ul>
           {eventLog.map((event, index) => (
-            <li key={index}>{event.name}: ~{event.end - event.start}ms</li>
+            <li key={index}>
+              {event.name}: ~{event.end - event.start}ms
+            </li>
           ))}
         </ul>
         <button onClick={clearEventLog}>Clear log</button>
